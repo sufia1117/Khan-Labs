eda4deb30db1ca3f939c343637c8ab792dd150f4
sufia1117 <sufia.khan1@marist.edu>
2023-01-24 10:04:26 -0500

    Commit with user input and printed conversion

--- Lab1/src/Selection.java
+++ Lab1/src/Selection.java
@@ -9,6 +9,7 @@ public class Selection {
             rand = random.nextInt(6);
             if (rand != 0) break;
         }
+
         // Assign each move to a number 1 to 5
         if (rand == 1) {
             String computerChoice = "rock";
@@ -24,6 +25,31 @@ public class Selection {
         }
         else if (rand == 5) {
             String computerChoice = "spock";
+        }
+
+        // Print numbers and corresponding moves for user
+        System.out.print("1) rock\n2) paper\n3)scissors\n4)lizard\n5)spock");
+        System.out.print("Enter a number corresponding to your preferred move: ");
+
+        // Ask user to choose a number
+        Scanner input = new Scanner(System.in);
+        int userChoice = input.nextInt();
+
+        // Print corresponding move to user's number choice
+        if (userChoice == 1){
+            System.out.print("You chose rock");
+        }
+        else if (userChoice == 2) {
+            System.out.print("You chose paper");
+        }
+        else if (userChoice == 3) {
+            System.out.print("You chose scissors");
+        }
+        else if (userChoice == 4) {
+            System.out.print("You chose lizard");
+        }
+        else if (userChoice == 5) {
+            System.out.print("You chose spock");
         }
     }
 }
