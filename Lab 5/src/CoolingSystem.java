02ece5e9b43453b35ad6ee9b2dadc3e85a6e2781
sufia1117 <sufia.khan1@marist.edu>
2023-03-09 13:37:53 -0500

    Complete Lab 5

--- Lab 5/src/CoolingSystem.java
+++ Lab 5/src/CoolingSystem.java
@@ -1,12 +1,16 @@
 public class CoolingSystem {
     private static Fan[] fans;
-    private int operatingCapacity = minCapacity;
+    public int operatingCapacity = minCapacity;
     private boolean on = false;
     private int systemSpeed = Fan.SLOW;
     private static final int minCapacity = 1;
 
     private static final int maxCapacity = 10;
 
+    /**
+     * Constructor for CoolingSystem which checks for validity
+     * @param numOfFans is the number of fans in a system
+     */
     public CoolingSystem(int numOfFans){
         if (numOfFans > maxCapacity){
             numOfFans = maxCapacity;
@@ -17,22 +21,58 @@ public class CoolingSystem {
         }
     }
 
+    /**
+     * Allows other classes to access the minimum capacity
+     * @return the minimum capacity, which is 1
+     */
+    public int getMinCapacity(){
+        return minCapacity;
+    }
+
+    /**
+     * Allows other classes to access the maximum capacity
+     * @return the maximum capacity, which is 10
+     */
+    public int getMaxCapacity(){
+        return maxCapacity;
+    }
+
+    /**
+     * Accessor to see if system is on
+     * @return boolean true for on and false for off
+     */
     public boolean isOn(){
         return on;
     }
 
+    /**
+     * Accessor to know overall speed of system
+     * @return overall speed of system
+     */
     public int getSpeed() {
         return systemSpeed;
     }
 
+    /**
+     * Accessor to see the number of fans operating when system is on
+     * @return number of fans on when system is on
+     */
     public int getOperatingCapacity(){
         return operatingCapacity;
     }
 
+    /**
+     * Accessor to check if system is operating at maximum or minimum capacity
+     * @return capacity at which system is operating
+     */
     public int getCapacity(){
         return operatingCapacity;
     }
 
+    /**
+     * Turns cooling system on or off
+     * @param on takes in boolean true for on anf false for off
+     */
     public void setOn(boolean on){
         this.on = on;
         if (this.on) {
@@ -50,17 +90,31 @@ public class CoolingSystem {
         }
     }
 
-    public void setOSystemSpeed(int systemSpeed){
+    /**
+     * Sets speed for overall system
+     * @param systemSpeed takes in SLOW, MEDIUM, or FAST from Fan class
+     */
+    public void setSystemSpeed(int systemSpeed){
         this.systemSpeed = systemSpeed;
     }
 
-    public void setOperatingCapacity(int operatingCapacity1){
-        operatingCapacity = Math.max(operatingCapacity, fans.length);
-        operatingCapacity = Math.min(operatingCapacity, fans.length);
-        this.operatingCapacity = operatingCapacity;
-
+    /**
+     * Sets the number of fans on when system is on; checks validity
+     * @param operatingCapacity takes in a number between 1 and 10
+     */
+    public void setOperatingCapacity(int operatingCapacity) {
+        if (operatingCapacity > fans.length) {
+            operatingCapacity = fans.length;
+        } else if (operatingCapacity < minCapacity) {
+            operatingCapacity = minCapacity;
+            this.operatingCapacity = operatingCapacity;
+        }
     }
 
+
+    /**
+     * Increments operating capacity by one 
+     */
     public void turnUp(){
         if (operatingCapacity == maxCapacity){
             this.operatingCapacity = maxCapacity;
@@ -69,6 +123,9 @@ public class CoolingSystem {
         }
     }
 
+    /**
+     * Decrements operating capacity by one
+     */
     public void turnDown(){
         if (operatingCapacity == minCapacity){
             this.operatingCapacity = minCapacity;
@@ -76,10 +133,4 @@ public class CoolingSystem {
             this.operatingCapacity = operatingCapacity - 1;
         }
     }
-
-
-
-
-
-
 }
--- Lab 5/src/CoolingSystemOperator.java
+++ Lab 5/src/CoolingSystemOperator.java
@@ -1,13 +1,47 @@
 public class CoolingSystemOperator {
-    public void testRampUp(CoolingSystem sys){
+    /**
+     * Increments operating capacity one level at a time to reach maximum capacity
+     * Decrements operating capacity one level at a time to reach minimum capacity
+     * @param sys takes in the specific cooling system
+     */
+    public static void testRampUp(CoolingSystem sys){
+        // Set operating capacity to 1
         sys.setOperatingCapacity(1);
+        // Turn system on
         sys.setOn(true);
-        for (int i = 0; i < maxCapacity; i++ ){
-
+        int operatingCapacity;
+        // Increment capacity until max capacity is reached
+        for (int i = 0; i < sys.getMaxCapacity(); i++ ){
+            operatingCapacity = sys.operatingCapacity + 1;
         }
+        // Increment capacity until minimum capacity is reached
+        for (int i = 0; i < sys.getMinCapacity(); i++){
+            operatingCapacity = sys.operatingCapacity - 1;
+        }
+        // Turn system off
+        sys.setOn(false);
     }
 
+    /**
+     * Tests system to make sure it will not allow invalid numbers of fans outside of minimum and max range
+     * @param sys takes in the specific cooling system
+     */
+    public static void testRange(CoolingSystem sys){
+        // Turn system on
+        sys.setOn(true);
+        // set capacity above valid range
+        sys.setOperatingCapacity(12);
+        // set capacity below valid range
+        sys.setOperatingCapacity(-1);
+        // turn system off
+        sys.setOn(false);
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        CoolingSystem systemOne = new CoolingSystem(8);
+        systemOne.setOn(true);
+        systemOne.setSystemSpeed(Fan.MEDIUM);
+        systemOne.setOperatingCapacity(4);
+        testRampUp(systemOne);
+        testRange(systemOne);
     }
 }
